4e4f60a18b7a314774f190cd55e43d6dc2624f98
Blayne Chard <blayne@chard.com>
2022-02-12 18:17:19 +1300

    refactor: apply formatter

--- src/lib.rs
+++ src/lib.rs
@@ -1,108 +1,108 @@
 // pub mod qk {
-    const CHAR_CODE_ZERO: usize = 48;
+const CHAR_CODE_ZERO: usize = 48;
 
-    #[derive(Debug, PartialEq)]
-    pub struct Tile {
-        pub x: usize,
-        pub y: usize,
-        pub z: usize,
+#[derive(Debug, PartialEq)]
+pub struct Tile {
+    pub x: usize,
+    pub y: usize,
+    pub z: usize,
+}
+#[derive(Debug, Clone, PartialEq)]
+pub struct QuadKeyParseError;
+
+pub fn tile_to_u64(x: usize, y: usize, zoom: usize) -> u64 {
+    let mut qk: u64 = 0;
+    let mut i = zoom;
+    while i > 0 {
+        let mask = 1 << (i - 1);
+        let location = 64 - ((zoom - i + 1) * 2) + 1;
+
+        if (x & mask) != 0 {
+            qk = qk | 1 << (location - 1);
+        }
+        if (y & mask) != 0 {
+            qk = qk | 1 << location;
+        }
+        i = i - 1;
     }
-    #[derive(Debug, Clone, PartialEq)]
-    pub struct QuadKeyParseError;
 
-    pub fn tile_to_u64(x: usize, y: usize, zoom: usize) -> u64 {
-        let mut qk: u64 = 0;
-        let mut i = zoom;
-        while i > 0 {
-            let mask = 1 << (i - 1);
-            let location = 64 - ((zoom - i + 1) * 2) + 1;
+    qk = qk | (zoom as u64);
+    return qk;
+}
 
-            if (x & mask) != 0 {
-                qk = qk | 1 << (location - 1);
-            }
-            if (y & mask) != 0 {
-                qk = qk | 1 << location;
-            }
-            i = i - 1;
+pub fn u64_to_tile(qk: u64) -> Tile {
+    let zoom = u64_zoom_level(qk) as usize;
+    let mut x = 0;
+    let mut y = 0;
+    let mut i = zoom;
+    while i > 0 {
+        let mask = 1 << (i - 1);
+
+        let location = 64 - ((zoom - i + 1) * 2) + 1;
+        if qk & (1 << (location - 1)) != 0 {
+            x = x | mask
+        }
+        if qk & (1 << location) != 0 {
+            y = y | mask
+        }
+        i = i - 1;
+    }
+
+    return Tile { x, y, z: zoom };
+}
+
+#[inline]
+pub fn u64_zoom_level(bin_qk: u64) -> u64 {
+    return bin_qk & 31;
+}
+
+pub fn tile_to_str(x: usize, y: usize, z: usize) -> String {
+    let mut res = vec!['0'; z];
+
+    let mut i = z;
+    while i > 0 {
+        let mut digit = '0' as u8;
+        let mask = 1 << (i - 1);
+        if (x & mask) != 0 {
+            digit = digit + 1;
+        }
+        if (y & mask) != 0 {
+            digit = digit + 2;
         }
 
-        qk = qk | (zoom as u64);
-        return qk;
+        res[z - i] = digit as char;
+
+        i = i - 1;
     }
+    return res.into_iter().collect();
+}
 
-    pub fn u64_to_tile(qk: u64) -> Tile {
-        let zoom = u64_zoom_level(qk) as usize;
-        let mut x = 0;
-        let mut y = 0;
-        let mut i = zoom;
-        while i > 0 {
-            let mask = 1 << (i - 1);
+pub fn str_to_tile(qk: String) -> Result<Tile, QuadKeyParseError> {
+    let mut x = 0;
+    let mut y = 0;
+    let z = qk.len();
 
-            let location = 64 - ((zoom - i + 1) * 2) + 1;
-            if qk & (1 << (location - 1)) != 0 {
-                x = x | mask
+    let bytes = qk.as_bytes();
+    let mut i = z;
+    while i > 0 {
+        let mask = 1 << (i - 1);
+
+        let q = (bytes[z - i] as usize) - CHAR_CODE_ZERO;
+        match q {
+            0 => (),
+            1 => x = x | mask,
+            2 => y = y | mask,
+            3 => {
+                x |= mask;
+                y |= mask;
             }
-            if qk & (1 << location) != 0 {
-                y = y | mask
-            }
-            i = i - 1;
+            _ => return Err(QuadKeyParseError {}),
         }
-
-        return Tile { x, y, z: zoom };
+        i = i - 1;
     }
 
-    #[inline]
-    pub fn u64_zoom_level(bin_qk: u64) -> u64 {
-        return bin_qk & 31;
-    }
-
-    pub fn tile_to_str(x: usize, y: usize, z: usize) -> String {
-        let mut res = vec!['0'; z];
-
-        let mut i = z;
-        while i > 0 {
-            let mut digit = '0' as u8;
-            let mask = 1 << (i - 1);
-            if (x & mask) != 0 {
-                digit = digit + 1;
-            }
-            if (y & mask) != 0 {
-                digit = digit + 2;
-            }
-
-            res[z - i] = digit as char;
-
-            i = i - 1;
-        }
-        return res.into_iter().collect();
-    }
-
-    pub fn str_to_tile(qk: String) -> Result<Tile, QuadKeyParseError> {
-        let mut x = 0;
-        let mut y = 0;
-        let z = qk.len();
-
-        let bytes = qk.as_bytes();
-        let mut i = z;
-        while i > 0 {
-            let mask = 1 << (i - 1);
-
-            let q = (bytes[z - i] as usize) - CHAR_CODE_ZERO;
-            match q {
-                0 => (),
-                1 => x = x | mask,
-                2 => y = y | mask,
-                3 => {
-                    x |= mask;
-                    y |= mask;
-                }
-                _ => return Err(QuadKeyParseError {}),
-            }
-            i = i - 1;
-        }
-
-        return Ok(Tile { x, y, z });
-    }
+    return Ok(Tile { x, y, z });
+}
 // }
 
 #[cfg(test)]
@@ -159,21 +159,15 @@ mod tests {
     #[test]
     fn it_converts_from_u64() {
         assert_eq!(
-            crate::u64_to_tile(
-                0b1100000000000000000000000000000000000000000000000000000000000001
-            ),
+            crate::u64_to_tile(0b1100000000000000000000000000000000000000000000000000000000000001),
             crate::Tile { x: 1, y: 1, z: 1 }
         );
         assert_eq!(
-            crate::u64_to_tile(
-                0b0010110101100100000000000000000000000000000000000000000000000111
-            ),
+            crate::u64_to_tile(0b0010110101100100000000000000000000000000000000000000000000000111),
             crate::Tile { x: 29, y: 50, z: 7 }
         );
         assert_eq!(
-            crate::u64_to_tile(
-                0b110001001001011111010100110011000000000000000000000000000010000
-            ),
+            crate::u64_to_tile(0b110001001001011111010100110011000000000000000000000000000010000),
             crate::Tile {
                 x: 35210,
                 y: 21493,
